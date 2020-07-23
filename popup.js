ace0d7bab17720e3aee3ec5395e4ebcab1bd8854
Darwin Sinchi <dsinchi12@gmail.com>
2020-07-22 22:26:09 -0400

    2buttons

--- popup.js
+++ popup.js
@@ -1,13 +1,34 @@
+//First Button
 let changeColor = document.getElementById("changeColor");
+changeColor.addEventListener("click", deleteEverything);
+let stackOverflow = document.getElementById("stackOverflow");
+stackOverflow.addEventListener("click", goToStack);
 
-chrome.storage.sync.get("color", function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute("value", data.color);
-  changeColor.addEventListener("click", deleteEverything);
-  function deleteEverything() {
-    chrome.windows.getAll({}, function(windows) {
-      for (var i = 0; i < windows.length; i++)
-        chrome.windows.remove(windows[i].id);
-    });
-  }
-});
+//changeColor.style.backgroundColor = data.color;
+//changeColor.setAttribute("value", data.color);
+
+//Delete the entire window
+function deleteEverything() {
+  chrome.windows.getAll({}, function(windows) {
+    for (var i = 0; i < windows.length; i++)
+      chrome.windows.remove(windows[i].id);
+  });
+}
+
+let workPages = [
+  "https://www.dummies.com/programming/java/javascript-for-dummies-cheat-sheet/",
+  "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  "https://reactjs.org/docs/getting-started.html",
+  "https://docs.mongodb.com/",
+  "https://brew.sh/"
+];
+
+function goToStack() {
+  //an
+  // var newURL =
+  //   "https://www.dummies.com/programming/java/javascript-for-dummies-cheat-sheet/";
+  let randomNumber = Math.floor(Math.random() * workPages.length);
+  let newURL = workPages[randomNumber];
+  console.log("it worked i guess");
+  chrome.tabs.create({ url: newURL });
+}
